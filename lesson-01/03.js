0a46a1944e0a1a7d6e21b20ae26b477439245fe1
DmitryKlimov92 <the.cabbagecake@gmail.com>
2024-03-28 00:44:46 +0300

    Merge remote-tracking branch 'origin/main'

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,29 +1,21 @@
-// code
+
+let number = 1;
+
+(number++);
+(++number);
+(number--);
+(--number);
+
+console.log( (number++),(','),(++number),(','), (number--),(','), (--number) );
+
+
+
+
 
 
 /*
-
-Имея в распоряжении только:
-
-let number = 1;
-и используя лишь
---number   number--  ++number   number++
-Получи в консоли 1,3,3,1
-
-❗Нужно вызвать консоль ЧЕТЫРЕ раза
-
-Пример:
-
-console.log(number**);
-console.log(**number);
-console.log(number**);
-console.log(**number); .
-
- */
-
-let number = 1;
-
 console.log(number++);
 console.log(++number);
 console.log(number--);
 console.log(--number);
+*/
