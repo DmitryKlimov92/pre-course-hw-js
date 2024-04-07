8b827ab3ab722452141eac4cb02e41924a41320c
DmitryKlimov92 <the.cabbagecake@gmail.com>
2024-04-07 14:29:29 +0300

    u

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -2,31 +2,13 @@ let passportMarried = {
     name: "Petr",
     surname: "Petrov",
     address: {
-        "country": "USA",
-        "city": "Bobryisk"
+        country: "USA",
+        city: "Bobryisk"
     }
 };
 
-let passportMarried2 = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        "country": "USA",
-        "city": "Bobryisk",
-        married: true
-    }
-};
+let passportMarried2 = { ...passportMarried };
+passportMarried2.married = true;
 
-/*
-
-const passportMarried2 = JSON.parse(JSON.stringify(passportMarried));
-passportMarried2.married = True;
-
- */
-
-/*
-let  passportMarried2 = structuredClone(passportMarried);
-passportMarried2.married = "True";
- */
 console.log(passportMarried);
 console.log(passportMarried2);
